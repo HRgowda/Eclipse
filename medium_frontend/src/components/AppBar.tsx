7595f8230ae5341924d8cb76154260b0b179e0a8
hemanth18 <hg510443@gmail.com>
2024-08-02 22:46:12 +0530

    Created AppBar component to the frontend

---
+++ medium_frontend/src/components/AppBar.tsx
@@ -0,0 +1,13 @@
+import { Avatar } from "./BlogCard"
+
+export const AppBar = () => {
+  return <div className="flex justify-between border-b px-10">
+    <div>
+      Eclipse
+    </div>
+    <div>
+      <Avatar name="Hemanth Gowda"></Avatar>
+    </div>
+    
+  </div>
+} 
