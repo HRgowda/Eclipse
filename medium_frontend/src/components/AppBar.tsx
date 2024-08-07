ac11eb026473fc6dda79c1b91e41a2c976c4fa64
hemanth18 <hg510443@gmail.com>
2024-08-07 20:11:23 +0530

    Make website title dynamic and add redirect to dashboard on click

--- medium_frontend/src/components/AppBar.tsx
+++ medium_frontend/src/components/AppBar.tsx
@@ -1,10 +1,10 @@
 import { Avatar } from "./BlogCard"
-
+import {Link} from "react-router-dom"
 export const AppBar = () => {
   return <div className="px-10 py-4 flex justify-between border-b">
-    <div className="flex flex-col justify-center font-semibold">
-      Eclipse
-    </div>
+    <Link to={'/blogs'} className="flex flex-col justify-center font-semibold cursor-pointer">
+      WordSmith
+    </Link>  
 
     <div>
       <Avatar size={"big"} name="Hemanth Gowda"></Avatar>
