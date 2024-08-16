cb4cbf166afa767eae03421172413c1fb729a70d
hemanth18 <hg510443@gmail.com>
2024-08-16 20:00:21 +0530

    Added Logout button to the Appbar

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import { Avatar } from "./BlogCard"
 import {Link} from "react-router-dom"
 export const AppBar = () => {
+
   return <div className="px-10 py-4 flex justify-between border-b">
     <Link to={'/blogs'} className="flex flex-col justify-center font-bold cursor-pointer ">
     MindCraft
@@ -10,6 +12,17 @@ export const AppBar = () => {
         
         <button type="button" className="mr-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">New Blog</button>
 
+      </Link>
+
+      {/* Logout button with functionality navigating to signin page by removing user token and name */}
+      <Link to={`/signin`}>
+        
+        <button onClick={()=>{
+          localStorage.removeItem("token");
+          localStorage.removeItem("name");
+          alert("Successfully logged out");
+        }}  type="button" className="mr-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Logout</button>
+      
       </Link>
 
       <Avatar size={"big"} name={"Hemanth Gowda"}></Avatar>
